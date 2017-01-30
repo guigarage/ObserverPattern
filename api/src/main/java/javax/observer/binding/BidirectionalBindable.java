a212185e19b9c067f2d834c82234c3efb185bd33
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-01-30 10:35:07 +0100

    doc

--- api/src/main/java/javax/observer/binding/BidirectionalBindable.java
+++ api/src/main/java/javax/observer/binding/BidirectionalBindable.java
@@ -4,6 +4,10 @@ package javax.observer.binding;
 import javax.observer.Property;
 import javax.observer.Subscription;
 
+/**
+ * Provides support for bidirectional bindings.
+ * @param <T>
+ */
 public interface BidirectionalBindable<T> extends Bindable<T> {
 
     Subscription bidirectionalTo(Property<T> property);
--- api/src/main/java/javax/observer/binding/ConvertableBindable.java
+++ api/src/main/java/javax/observer/binding/ConvertableBindable.java
@@ -2,11 +2,24 @@ package javax.observer.binding;
 
 import javax.observer.Observable;
 import javax.observer.Subscription;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
+/**
+ * Adds the support of different type bindings to the {@link Bindable} interface. This interface
+ * offers bindings between different value types by defining {@link Function} based converters.
+ * @param <T> value type
+ */
 public interface ConvertableBindable<T> extends Bindable<T> {
 
+    /**
+     * By defining a converter a unidirectional binding to an {@link Observable} with a different value
+     * type can be defined. For more information about binding see {@link Bindable#to(Observable)}
+     * @param observable the {@link Observable} to that this instance will be bound.
+     * @param converter the converter that will be used to convert any new value of the {@link Observable}
+     *                  to the correct value type
+     * @param <U> value type of the {@link Observable} to that this instance will be bound.
+     * @return a {@link Subscription} to unbind the binding that is created by calling this method
+     */
     <U> Subscription to(Observable<U> observable, Function<U, T> converter);
 
     @Override
@@ -14,5 +27,4 @@ public interface ConvertableBindable<T> extends Bindable<T> {
         return to(observable, v -> v);
     }
 
-    ConvertableBindable<T> withErrorHandler(Consumer<Throwable> handler);
 }
